aca7a6893fb1ec1b95f7f27c774bfcbd3e436b1a
jiangzhi.xie <xiejiangzhi@gmail.com>
2016-10-06 14:14:07 +0800

    Optimize TileMap, support big map

--- tile_map/main.lua
+++ tile_map/main.lua
@@ -4,6 +4,10 @@ tiles = {}
 
 game_info = {w = 0, h = 0}
 
+-- map[x][y]
+--  nil: empty
+--  -1: edge
+--  >=1: tile
 map = {}
 map_info = {
   x = 0,
@@ -13,13 +17,18 @@ map_info = {
   w = nil,
   h = nil,
 
-  cols = 100,
-  rows = 20,
+  cols = 9000,
+  rows = 300,
   tile_w = 50,
   tile_h = 50,
   tile_scale_x = 1,
   tile_scale_y = 1,
-  world = nil
+  world = nil,
+
+  draw_sx = 0,
+  draw_sy = 0,
+  draw_ex = 0,
+  draw_ey = 0
 }
 
 player = {speed = 400, x = 100, y = 100, can_jump = true, w = 0, h = 0}
@@ -47,6 +56,11 @@ function love.draw()
   love.graphics.print(
     "move with Left and Right, jump with Space, destroy tile with Down", 50, 50
   )
+  love.graphics.print(
+    math.floor(player.x / map_info.tile_w) .. ', ' .. math.floor(player.y / map_info.tile_h),
+    50, 100
+  )
+  love.graphics.print("World body count: " .. map_info.world:getBodyCount(), 50, 150)
   draw_player()
 end
 
@@ -54,6 +68,8 @@ end
 function init_game()
   game_info.w = love.graphics.getWidth()
   game_info.h = love.graphics.getHeight()
+  game_info.cw = game_info.w / 2
+  game_info.ch = game_info.h / 2
 end
 
 function init_tiles()
@@ -77,32 +93,21 @@ function init_map()
   map_info.w = map_info.tile_w * map_info.cols
   map_info.h = map_info.tile_h * map_info.rows
 
-  local halt_tile_w = map_info.tile_w / 2
-  local halt_tile_h = map_info.tile_h / 2
 
   for y = 0, map_info.rows - 1 do
     for x = 0, map_info.cols - 1 do
       if not map[x] then map[x] = {} end
       local is_edge = x == 0 or y == 0 or x == (map_info.cols - 1) or y == (map_info.rows - 1)
-      if (y > map_info.rows / 2) or is_edge then
+      if (y > map_info.rows / 5) or is_edge then
         local game_x = x * map_info.tile_w
         local game_y = y * map_info.tile_h
         local tile = {} 
-        if not is_edge then
+        if is_edge then
+          tile.type = -1
+        else
           tile.type = math.random(tiles_total)
         end
-        tile.body = love.physics.newBody(
-          map_info.world, game_x + halt_tile_w, game_y + halt_tile_h
-        )
-        tile.shape = love.physics.newRectangleShape(map_info.tile_w, map_info.tile_h)
-        tile.fixture = love.physics.newFixture(tile.body, tile.shape)
-        if is_edge then
-          tile.fixture:setUserData('edge')
-          tile.body:setUserData({x = x, y = y, type = 'edge'})
-        else
-          tile.fixture:setUserData('tile')
-          tile.body:setUserData({x = x, y = y, type = 'tile'})
-        end
+        
         map[x][y] = tile
       end
     end
@@ -114,6 +119,7 @@ function init_player()
   player.h = map_info.tile_h * 2
 
   player.body = love.physics.newBody(map_info.world, player.x, player.y, 'dynamic')
+  player.body:setUserData({type = 'player'})
   player.shape = love.physics.newRectangleShape(player.w, player.h)
   player.fixture = love.physics.newFixture(player.body, player.shape)
   player.fixture:setUserData('player')
@@ -123,24 +129,22 @@ function draw_map()
   love.graphics.translate(map_info.x, map_info.y)
   love.graphics.setBackgroundColor(150, 150, 250)
 
-  for map_y = 0, map_info.rows - 1 do
-    for map_x = 0, map_info.cols - 1 do
-      local tile = map[map_x][map_y]
+  each_visible_tiles(function(map_x, map_y, tile)
+    if tile then
+      local game_x = map_x * map_info.tile_w
+      local game_y = map_y * map_info.tile_h
 
-      if tile then
-        local game_x = map_x * map_info.tile_w
-        local game_y = map_y * map_info.tile_h
-        if tile.type then
-          love.graphics.draw(
-            tiles[tile.type], game_x, game_y, 0, map_info.tile_scale_x, map_info.tile_scale_x
-          )
-        else
-          love.graphics.setColor(100, 100, 100)
-          love.graphics.rectangle('fill', game_x, game_y, map_info.tile_w, map_info.tile_h)
-        end
+      if tile.type > 0 then
+        love.graphics.setColor(255, 255, 255)
+        love.graphics.draw(
+          tiles[tile.type], game_x, game_y, 0, map_info.tile_scale_x, map_info.tile_scale_x
+        )
+      else -- edge
+        love.graphics.setColor(100, 100, 100)
+        love.graphics.rectangle('fill', game_x, game_y, map_info.tile_w, map_info.tile_h)
       end
     end
-  end 
+  end)
 
   love.graphics.origin()
 end
@@ -176,7 +180,7 @@ function check_move(dt)
             local data = fixture:getBody():getUserData()
             local tile = map[data.x][data.y]
             if tile then
-              tile.body:destroy()
+              destroy_world_tile(tile)
               map[data.x][data.y] = nil
             end
           end
@@ -202,6 +206,17 @@ function update_world(dt)
   player.x = player.body:getX()
   player.y = player.body:getY()
 
+  map_info.draw_sx = math.max(math.floor((player.x - game_info.w) / map_info.tile_w), 0)
+  map_info.draw_sy = math.max(math.floor((player.y - game_info.h) / map_info.tile_h), 0)
+  map_info.draw_ex = math.min(
+    math.ceil((player.x + game_info.w) / map_info.tile_w), map_info.cols - 1
+  )
+  map_info.draw_ey = math.min(
+    math.ceil((player.y + game_info.h) / map_info.tile_h), map_info.rows -1
+  )
+
+  reset_map_bodys()
+    
   move_map()
 end
 
@@ -217,5 +232,65 @@ function beginContact(a, b, coll)
 end
 
 function engContact(a, b, coll)
+end
+
+function each_visible_tiles(fn)
+  for y = map_info.draw_sy, map_info.draw_ey do
+    for x = map_info.draw_sx, map_info.draw_ex do
+      local tile = map[x][y]
+      fn(x, y, tile)
+    end
+  end
+end
+
+function reset_map_bodys()
+  local halt_tile_w = map_info.tile_w / 2
+  local halt_tile_h = map_info.tile_h / 2
+
+  each_visible_tiles(function(map_x, map_y, tile)
+    if tile then
+      local game_x = map_x * map_info.tile_w
+      local game_y = map_y * map_info.tile_h
+      tile.save_body = true
+
+      if not tile.body then
+        tile.save_body = true
+        tile.body = love.physics.newBody(
+          map_info.world, game_x + halt_tile_w, game_y + halt_tile_h
+        )
+        tile.shape = love.physics.newRectangleShape(map_info.tile_w, map_info.tile_h)
+        tile.fixture = love.physics.newFixture(tile.body, tile.shape)
+
+        if tile.type > 0 then
+          tile.fixture:setUserData('tile')
+          tile.body:setUserData({x = map_x, y = map_y, type = 'tile'})
+        else
+          tile.fixture:setUserData('edge')
+          tile.body:setUserData({x = map_x, y = map_y, type = 'edge'})
+        end
+      end
+    end
+  end)
+
+  for i, body in pairs(map_info.world:getBodyList()) do
+    local data = body:getUserData()
+    if data.type ~= 'player' then
+      local tile = map[data.x][data.y]
+
+      if tile.save_body then
+        tile.save_body = nil
+      else
+        destroy_world_tile(tile)
+      end
+    end
+  end
+end
+
+function destroy_world_tile(tile)
+  tile.body:destroy()
+  tile.body = nil
+  tile.shape = nil
+  tile.fixture = nil
+  tile.save_body = nil
 end
 
